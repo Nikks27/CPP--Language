b587251c0b0d110f2a9913544509419bdc9512d5
RABARI NARESH <148762716+Nikks27@users.noreply.github.com>
2024-01-30 14:58:08 +0530

    Add files via upload

---
+++ exam 1/1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Second
+{
+   int hh,mm,ss,n;
+   
+   void input()
+   {
+   	 mm=(n%3600)/60;
+   	 hh=/60
+   	 
+   } 	
+};
+
+main()
+{
+	int a,n;
+	class Second a[n];
+	
+}
+
---
+++ exam 1/2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Feet
+{
+   	int feet,inch;
+   	void input()
+   	{
+	   
+     	cout << "enter the feet";
+   	    cin >> feet;
+     	cout << "enter the inch";
+   	    cin >> inch;
+   	
+   	    feet =feet(inch/12);
+   	    inch=inch/12;
+   }
+};
+main()
+{
+	class Feet n[1];
+	
+}
